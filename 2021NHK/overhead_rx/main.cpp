7f0b5fb524a8d5cc7abd142e66938deb8dabf723
nalinally <61955594+nalinally@users.noreply.github.com>
2021-10-17 10:47:22 +0900

    Update main.cpp

--- 2021NHK/overhead_rx/main.cpp
+++ 2021NHK/overhead_rx/main.cpp
@@ -1,1 +1,37 @@
+#include "mbed.h"
 
+PwmOut servo[2] = {
+    PwmOut(PA_7),
+    PwmOut(PA_6)
+};
+
+Serial PC(USBTX, USBRX);
+Serial Xbee(PA_9, PA_10);
+
+int main(void)
+{
+    PC.printf("Program Written.");
+    int sign = 'a';
+    Xbee.baud(9600);
+    servo[0].pulsewidth(0.0005);
+    servo[1].pulsewidth(0.0025);
+    while(1){
+        if(Xbee.readable()){
+            sign = Xbee.getc();
+            PC.printf("%d", sign);
+            if(sign == 252){
+                wait(0.97);
+                servo[0].pulsewidth(0.0025);
+                wait(0.03);
+                servo[1].pulsewidth(0.0005);
+                PC.printf("Kick!");
+            }
+            else if(sign == 255){
+                servo[0].pulsewidth(0.0005);
+                servo[1].pulsewidth(0.0025);
+                PC.printf("Lock!");
+            }
+        }
+        wait(0.01);
+    }
+}
