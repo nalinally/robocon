e40e87c9a7dc1cf5383b83c831d5393a0538d72f
nalinally <61955594+nalinally@users.noreply.github.com>
2021-10-17 10:50:32 +0900

    Update main.cpp

--- 2021NHK/overhead_tx/main.cpp
+++ 2021NHK/overhead_tx/main.cpp
@@ -1,1 +1,30 @@
+#include "mbed.h"
 
+DigitalIn sw(PA_7);
+Serial PC(USBTX, USBRX);
+Serial Xbee(PA_9, PA_10);
+
+int main(void)
+{
+    int i;
+    sw.mode(PullDown);
+    Xbee.baud(9600);
+    while(1){
+        i = sw;
+        if(i == 0){
+            PC.printf("Kick!\n\r");
+            Xbee.printf("e");
+            while(i == 0){
+                i = sw;
+            }
+        }
+        else if(i == 1){
+            PC.printf("Lock!\n\r");
+            Xbee.putc('x');
+            while(i == 1){
+                i = sw;
+            }
+        }
+    }
+        
+}
